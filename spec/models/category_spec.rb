8b3c9b7b957a89a39443791af2634216f5a4b194
Mata Angelou <pk60905@hotmail.com>
2017-08-03 18:00:07 +0800

    Added test for category model, video model and association between the 2 models

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "save itself" do
+    category = Category.create(name: "Comedy")
+    expect(Category.first).to eq category
+  end
+
+  it "has many vidoes" do
+    comedy = Category.create(name: "Comedy")
+    hang_over = Video.create(title: "Hang Over", description: "Some content here", category: comedy)
+    role_models = Video.create(title: "Role Models", description: "Some content here", category: comedy)
+    expect(comedy.videos).to include hang_over, role_models
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Video do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should save the video content" do
+    video = Video.new(title: "A comdedy", description: "Some content here")
+    video.save
+    expect(Video.first).to eq video
+  end
+
+  it "belongs to one category" do
+    category = Category.create(name: "Comedy")
+    video = Video.create(title: "A comdedy", description: "Some content here", category: category)
+    expect(video.category).to eq category
+  end
 end
