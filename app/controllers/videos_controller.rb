eb335c7fbb352cec4216a05e061849ceae747b3a
Mata Angelou <pk60905@hotmail.com>
2017-08-03 17:05:45 +0800

    Added vategory model and made category show page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,9 +1,11 @@
 class VideosController < ApplicationController
   def index
+    @categories = Category.all
     @videos = Video.all
   end
 
   def show
-    
+    # binding.pry
+    @video = Video.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get '/home', to: 'videos#index'
+  # get '/videos/:id', to: 'videos#show'
+  resources :videos, except: [:index]
+  resources :categories, only: [:show]
 end
