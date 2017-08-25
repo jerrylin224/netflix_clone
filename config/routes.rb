a183ff8f12cf2d88fdcc31e83748365382e2e7e6
Mata Angelou <pk60905@hotmail.com>
2017-08-25 12:20:01 +0800

    add slash for routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,26 +13,26 @@ Myflix::Application.routes.draw do
   resources :queue_items, only: [:create, :destroy]
   post 'update_queue', to: 'queue_items#update_queue'
 
-  get 'ui(/:action)', controller: 'ui'
-  get 'home', to: 'videos#index'
+  get '/ui(/:action)', controller: 'ui'
+  get '/home', to: 'videos#index'
   # get '/videos/:id', to: 'videos#show'
 
   resources :relationships, only: [:create, :destroy]
-  get 'people', to: 'relationships#index'
+  get '/people', to: 'relationships#index'
 
-  get 'register', to: 'users#new'
-  get 'register/:token', to: 'users#new_with_invitation_token', as: 'register_with_token'
-  get 'sign_in', to: 'sessions#new'
-  post 'sign_in', to: 'sessions#create'
-  get 'sign_out', to: 'sessions#destroy'
-  get 'my_queue', to: 'queue_items#index'
+  get '/register', to: 'users#new'
+  get '/register/:token', to: 'users#new_with_invitation_token', as: 'register_with_token'
+  get '/sign_in', to: 'sessions#new'
+  post '/sign_in', to: 'sessions#create'
+  get '/sign_out', to: 'sessions#destroy'
+  get '/my_queue', to: 'queue_items#index'
 
-  get 'forgot_password', to: 'forgot_passwords#new'
+  get '/forgot_password', to: 'forgot_passwords#new'
   resources :forgot_passwords, only: [:create]
-  get 'forgot_password_confirmation', to: 'forgot_passwords#confirm'
+  get '/forgot_password_confirmation', to: 'forgot_passwords#confirm'
 
   resources :password_resets, only: [:show, :create]
-  get 'expired_token', to: 'pages#expired_token'
+  get '/expired_token', to: 'pages#expired_token'
 
   resources :invitations, only: [:new, :create]
 
