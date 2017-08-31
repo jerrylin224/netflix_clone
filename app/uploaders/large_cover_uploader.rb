75635f5bd85342f8847240657d0d0ba05a6d63cc
Mata Angelou <pk60905@hotmail.com>
2017-08-31 17:24:06 +0800

    Add storage:fog in uploader

--- app/uploaders/large_cover_uploader.rb
+++ app/uploaders/large_cover_uploader.rb
@@ -1,5 +1,11 @@
 class LargeCoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  storage :fog
+
   process resize_to_fit: [665, 375]
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 end
--- app/uploaders/small_cover_uploader.rb
+++ app/uploaders/small_cover_uploader.rb
@@ -1,5 +1,11 @@
 class SmallCoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  storage :fog
+
   process resize_to_fit: [166, 236]
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 end
