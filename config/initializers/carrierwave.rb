7fc541cd5785885dcceefc343b899850f1d9e03c
Mata Angelou <pk60905@hotmail.com>
2017-08-31 18:15:56 +0800

    Change fecth environment data to use bracket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,12 +7,12 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws' 
     config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV.fetch('AWS_ACCESS_KEY_ID'),
-      :aws_secret_access_key  => ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
       :region                 => 'ap-northeast-1' # Tokyo
     }
     config.storage = :fog
-    config.fog_directory  = ENV.fetch('S3_BUCKET_NAME')
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
 
   else
     config.storage = :file
